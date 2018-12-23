ea5dfb9ae60085b482f96024434801049d70628d
WICAdolpH <1650959546@qq.com>
2018-12-23 17:39:24 +0800

    Update config/wxxcx.php

--- config/wxxcx.php
+++ config/wxxcx.php
@@ -4,11 +4,11 @@ return [
 	/**
 	 * 小程序APPID
 	 */
-    'appid' => '',
+    'appid' => 'wxeb3d714215c2c7ad',
     /**
      * 小程序Secret
      */
-    'secret' => '',
+    'secret' => '084cd0b32863f76d6538c090eeeec645',
     /**
      * 小程序登录凭证 code 获取 session_key 和 openid 地址，不需要改动
      */
